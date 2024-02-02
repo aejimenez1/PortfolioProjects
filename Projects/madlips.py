e0b5b84e4ce70abada57e91df8ee79cba1c77f3b
Andres Jimenez <aejimenezmendoza97@gmail.com>
2024-02-01 19:12:33 -0500

    Adicion de carpeta Projects de python

---
+++ Projects/madlips.py
@@ -0,0 +1,10 @@
+print("Welcome to the madlibs game! Please write some words")
+adj = input("Adjetive: ")
+verb1 = input("First verb: ")
+verb2 = input("Second verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all time because \
+I love to {verb1}. Stay hidrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
